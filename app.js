548cc05b9b308f9ea61814191b4ad9657ed29d01
Nicholas Ewing <nicholassewing@gmail.com>
2018-09-15 01:51:49 +0000

    added schema, blog post route, and new blog form

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+mongoose.connect("mongodb://localhost/blogcms", {useNewUrlParser: true});
+mongoose.set("useCreateIndex", true);
 app.set("view engine", "ejs");
 
+
 app.get("/", function(req, res){
+	res.redirect("blogs");
+})
+
+app.get("/blogs", function(req, res){
 	res.render("blog");
 });
 
-// set up mongodb
+app.get("/blogs/new", function(req, res){
+	res.render("new");
+});
+
+app.post("/blogs", function(req, res){
+	// some magic that adds the post to db
+});
+
+// blog schema
+let Blog = mongoose.model("Blog", {
+	title: { type: String, unique: true },
+	body: String,
+});
+
+// let firstPost = new Blog({
+// 	title: "This is the title",
+// 	body: "This is some body text."
+// });
+
+// firstPost.save();
 
 app.listen(3000, () => console.log("Server started on port 3000"));
