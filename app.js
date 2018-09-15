87c6b3b227ff2bd5bc70518529d1728bd757ae73
Nicholas Ewing <nicholassewing@gmail.com>
2018-09-15 15:55:33 +0000

    truncated blog post preview to 50 characters

--- app.js
+++ app.js
@@ -45,16 +45,22 @@ app.post("/blogs", function(req, res){
 	});
 });
 
-// add show route
-
 app.get("/blogs/:id", function(req, res){
 	Blog.findById(req.params.id, function(err, foundBlog){
 		if(err){
 			res.redirect("/blogs")
 		}
-
 		res.render("show", {blog: foundBlog});
 	})
 });
+
+// add edit template
+// add update route
+// add destroy route
+// add error handling
+// add images to blog posts
+// sanitize input for blogs
+// add login system
+// add ability to comment
 
 app.listen(3000, () => console.log("Server started on port 3000"));
