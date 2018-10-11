be6d339168a2b3a3b884547bd5d03e36a469d874
Nicholas Ewing <nicholassewing@gmail.com>
2018-10-11 23:19:18 +0000

    Created new comment form

--- routes/comments.js
+++ routes/comments.js
@@ -1,14 +1,26 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
-
-const router = express.Router();
-
-let Comments = require("../models/comment");
+const router = express.Router({mergeParams: true});
+const Blog = require("../models/blog");
+const Comment = require("../models/comment");
 
 router.get("/new", function(req, res){
-	res.render("comments/new");
+	Blog.findById(req.params.id, function(err, blog){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("comments/new", {blog: blog});
+		}
+	});
 });
+
+router.post("/", function(req, res){
+	// blog findbyid
+	// create comment
+		// push comments
+		// save blog
+		// redirect
+});
+
+
 
 module.exports = router;
