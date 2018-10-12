d58693a54e2f96c7c52bff43d8371371cae04e97
Nicholas Ewing <nicholassewing@gmail.com>
2018-10-12 18:22:33 +0000

    Added ability to post comments. Styled comments & new comment template.

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.get("/", function(req, res){
 	res.redirect("/blogs");
 });
 
-app.get("/secret", isLoggedIn ,function(req, res){
+app.get("/secret", isLoggedIn, function(req, res){
 	res.render("secret")
 })
 
--- routes/comments.js
+++ routes/comments.js
@@ -1,8 +1,10 @@
 const express = require("express");
+// Look up what mergeParams does and study it!
 const router = express.Router({mergeParams: true});
 const Blog = require("../models/blog");
 const Comment = require("../models/comment");
 
+// require login middleware for all routes
 router.get("/new", function(req, res){
 	Blog.findById(req.params.id, function(err, blog){
 		if(err){
@@ -14,12 +16,29 @@ router.get("/new", function(req, res){
 });
 
 router.post("/", function(req, res){
+	Blog.findById(req.params.id, function(err, blog){
+		if(err){
+			console.log(err);
+			res.redirect("/blogs");
+		} else {
+			Comment.create(req.body.comment, function(err, comment){
+				if(err){
+					console.log(err);
+					res.redirect("/blogs");
+				} else {
+					blog.comments.push(comment);
+					blog.save();
+					res.redirect("/blogs/" + blog._id);					
+				}
+			});
+		}
+	});
+});
 	// blog findbyid
 	// create comment
 		// push comments
 		// save blog
 		// redirect
-});
 
 
 
