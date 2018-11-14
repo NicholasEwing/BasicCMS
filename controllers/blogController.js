5897ca000d67d7ffc696567cd905c7c000bbe2f8
Nicholas Ewing <nicholassewing@gmail.com>
2018-11-14 16:42:54 +0000

    Refactor all Blog methods to promises

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -5,14 +5,14 @@ const Comment = require("../models/comment");
 module.exports = {
 	getBlogs : (req, res) => {
 		Blog.find({})
-		.exec()
-		.then((blogs) => {
-			res.render("blogs/index", {blogs: blogs});
-		})
-		.catch((err) => {
-			req.flash("error toast", "Error retrieving blogs.");
-			res.redirect("/");
-		});
+			.exec()
+			.then((blogs) => {
+				res.render("blogs/index", {blogs: blogs});
+			})
+			.catch((err) => {
+				req.flash("error toast", "Error retrieving blogs.");
+				res.redirect("/");
+			});
 	},
 	newBlogForm : (req, res) => {
 		res.render("blogs/new");
@@ -20,99 +20,72 @@ module.exports = {
 	createBlog : (req, res) => {
 		req.body.blog.body = req.sanitize(req.body.blog.body);
 
-		// rewrite this stuff below as a promise!
-
 		Blog.create(req.body.blog)
 			.then((newBlog) => {
 				newBlog.author.id = req.user._id;
 				newBlog.author.username = req.user.username;
-				newBlog.save()
+				return newBlog.save();
 			})
-			.then((createdBlog) => User.findOneAndUpdate(req.user._id, {$push: {blogs: createdBlog}}))
+			.then((createdBlog) => {
+				User.findOneAndUpdate(req.user._id, {$push: {blogs: createdBlog._id}})
+					.exec();
+			})
 			.then(() => res.redirect("/blogs"))
 			.catch((err) => {
-				res.flash("Whoops! Couldn't create new blog");
+				req.flash("error toast", "Whoops! Couldn't create new blog");
 				res.redirect("/blogs");
 			});
-
 	},
 	getBlog : (req, res) => {
-		Blog.findById(req.params.id).populate("comments").exec((err, foundBlog) => {
-			if(err){
-				req.flash("error toast", "That blog post does not exist.");
+		Blog.findById(req.params.id)
+			.populate("comments")
+			.exec()
+			.then(foundBlog => res.render("blogs/show", {blog: foundBlog}))
+			.catch((err) => {
+				req.flash("error toast", "Couldn't find this blog post.")
 				res.redirect("/blogs");
-			}
-			res.render("blogs/show", {blog: foundBlog});
-		});
+			});
 	},
 	updateBlogForm : (req, res) => {
-		Blog.findById(req.params.id, (err, foundBlog) => {
-		if(err){
-			req.flash("error toast", "Couldn't open the edit view for this post. Please try again.");
-			res.redirect("/blogs");
-		}
-
-		if(!foundBlog){
-			req.flash("error toast", "That blog post does not exist.");
-			res.redirect("/blogs");			
-		}
-		
-		res.render("blogs/edit", {blog: foundBlog});
-		});
+		Blog.findById(req.params.id)
+			.then(foundBlog => res.render("blogs/edit", {blog: foundBlog}))
+			.catch((err) => {
+				req.flash("error toast", "An error occurred. Please try again.")
+				res.redirect("/blogs");
+			});
 	},
 	updateBlog : (req, res) => {
 		req.body.blog.body = req.sanitize(req.body.blog.body);
-		Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
-			if(err){
+
+		Blog.findOneAndUpdate(req.params.id, req.body.blog)
+			.then((updatedBlog) => {
+				res.redirect("/blogs/" + req.params.id);
+			})
+			.catch((err) => {
 				req.flash("error toast", "Failed to update blog. Please try again.");
 				res.redirect("/blogs");
-			}
-
-			if(!updatedBlog){
-				req.flash("error toast", "That blog post does not exist.");
-				res.redirect("/blogs");	
-			}
-		
-			res.redirect("/blogs/" + req.params.id);
-		});
+			});
 	},
 	deleteConfirm : (req, res) => {
 		res.render("blogs/delete", {blog_id: req.params.id});
 	},
 	deleteBlog : (req, res) => {
-		Blog.findById(req.params.id, (err, blog) => {
-			if(err){
-				req.flash("error toast", "Failed to delete blog.");
-			}
 
-			if(!blog){
-				req.flash("error toast", "Blog no longer exists.");
-			}
-
-			Comment.find({_id: {$in: blog.comments}}, (err, comments) => {
-				if(err) {
-					req.flash("error toast", "Error pulling comments when deleting blog.");
-					res.redirect("/");
-				}
-
-				User.updateMany({$pull: {comments: {$in: comments}}}, (err, users) => {
-					if(err) {
-						req.flash("error toast", "Error pulling comments from users.");
-						res.redirect("/");
-					}
-				});
-			});
-
-			User.updateMany({$pull: {blogs: {$in: blog._id}}}, (err, user) => {
-				if(err) {
-					req.flash("error toast", "Error pulling blog from author.")
-					res.redirect("/");
-				}
-			});
-
-			blog.remove();
-			req.flash("success toast", "Blog removed.");
-			res.redirect("/");
-		});
+		Blog.findById(req.params.id)
+			.then((blog) => {
+				blog.remove();
+				User.findOneAndUpdate(blog.author.id, {$pull: {blogs: blog._id}}).exec();
+				return Comment.find({_id: {$in: blog.comments}}).exec();
+			})
+			.then((comments) => {
+				// find comments in blog, pull comments out of all users
+				User.updateMany({$pull: {comments: {$in: comments}}}).exec();
+				Comment.remove({_id: {$in: comments._id}});
+			})
+			.then(() => {
+				req.flash("success toast", "Blog removed.");
+				res.redirect("/");
+			})
+			.catch(err => console.log(err));
 	}
 }
