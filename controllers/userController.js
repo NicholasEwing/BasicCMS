60388b19ae1ca441e79e4fd2e7bfe4b03805dfca
Nicholas Ewing <nicholassewing@gmail.com>
2018-11-15 17:38:11 +0000

    Refactor user controller to use promises

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,25 +5,23 @@ const ejs_helpers = require("../public/js/ejs_helpers.js");
 
 module.exports = {
 	getUsers : (req, res) => {
-		User.find({}, (err, users) => {
-		if(err){
-			req.flash("error toast", "Unable to render blog posts. Please try again later.");
-			res.redirect("/blogs");
-		}
-
-		res.render("users/index", {users: users});
-		});
+		User.find({})
+			.then((users) => res.render("users/index", {users: users}))
+			.catch((err) => {
+				console.log(err);
+				req.flash("error toast", "Unable to render users. Please try again later.");
+				res.render("users/index", {users: users});
+			});
 	},
 	getUser : (req, res) => {
 		User.findOne({username: req.params.id})
-		.populate("blogs")
-		.exec((err, results) => {
-			if(err || !results){
-				req.flash("error toast", "That user does not exist.");
-				res.redirect("/blogs");
-			}
-			
-			res.render("users/show", {helpers: ejs_helpers, results: results});
-		});
+			.populate("blogs")
+			.exec()
+			.then((results) => {res.render("users/show", {helpers: ejs_helpers, results: results})})
+			.catch((err) => {
+				console.log(err);
+				req.flash("error toast", "Unable to render user info. Please try again later.");
+				res.render("users/show", {helpers: ejs_helpers, results: results});
+			});
 	}
 }
