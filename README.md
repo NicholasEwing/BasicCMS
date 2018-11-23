ee54eb819688390a2c2def039eaf9110c896c610
Nicholas Ewing <nicholassewing@gmail.com>
2018-11-23 23:35:49 +0000

    Rewrite blog controller to use async/await

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [Click here to try a live demo!](https://basic-cms-by-nicholas-ewing.herokuapp.com)
 
-In this project, I've created a simple website where you can post blogs and comment on other users' blogs.
+In this project, I've created a simple app where you can post blogs and comment on other users' blogs.
 
 Technologies used:
 * HTML5
--- controllers/blogController.js
+++ controllers/blogController.js
@@ -4,92 +4,82 @@ const Comment = require("../models/comment");
 const mongoose = require("mongoose");
 
 module.exports = {
-	getBlogs : (req, res) => {
-		Blog.find({})
-			.exec()
-			.then((blogs) => {
-				res.render("blogs/index", {blogs: blogs});
-			})
-			.catch((err) => {
-				req.flash("error toast", "Error retrieving blogs.");
-				res.redirect("/");
-			});
+	getBlogs : async (req, res) => {
+		try {
+			const blogs = await Blog.find({});
+			return res.render("blogs/index", {blogs: blogs});
+		} catch(err) {
+			req.flash("error toast", "Error retrieving blogs.");
+			res.redirect("/");
+		}
 	},
 	newBlogForm : (req, res) => {
 		res.render("blogs/new");
 	},
-	createBlog : (req, res) => {
+	createBlog : async (req, res) => {
 		req.body.blog.body = req.sanitize(req.body.blog.body);
 
-		Blog.create(req.body.blog)
-			.then((newBlog) => {
-				newBlog.author.id = req.user._id;
-				newBlog.author.username = req.user.username;
-				return newBlog.save();
-			})
-			.then((createdBlog) => {
-				User.findByIdAndUpdate(req.user._id, {$push: {blogs: createdBlog._id}})
-					.exec();
-			})
-			.then(() => res.redirect("/blogs"))
-			.catch((err) => {
-				req.flash("error toast", "Whoops! Couldn't create new blog");
-				res.redirect("/blogs");
-			});
+		try {
+			const newBlog = await Blog.create(req.body.blog);
+			newBlog.author.id = req.user._id;
+			newBlog.author.username = req.user.username;
+			await newBlog.save();
+			await User.findByIdAndUpdate(req.user._id, {$push: {blogs: newBlog._id}});
+			res.redirect("/blogs");
+		} catch(err) {
+			req.flash("error toast", "Whoops! Couldn't create new blog.");
+		}
 	},
-	getBlog : (req, res) => {
-		Blog.findById(req.params.id)
-			.populate("comments")
-			.exec()
-			.then((foundBlog) => {
-				if(foundBlog) {
-					return res.render("blogs/show", {blog: foundBlog});
-				}
-
-				req.flash("error toast", "Couldn't find this blog post.");
-				return res.redirect("/");
-			});
+	getBlog : async (req, res) => {
+		try {
+			const foundBlog = await Blog.findById(req.params.id);
+			if(foundBlog) return res.render("blogs/show", {blog: foundBlog});
+			res.redirect("/");
+		} catch(err) {
+			req.flash("error toast", "Couldn't find this blog post.");
+			res.redirect("/");
+		}
 	},
-	updateBlogForm : (req, res) => {
-		Blog.findById(req.params.id)
-			.then(foundBlog => res.render("blogs/edit", {blog: foundBlog}))
-			.catch((err) => {
-				req.flash("error toast", "An error occurred. Please try again.")
-				res.redirect("/blogs");
-			});
+	updateBlogForm : async (req, res) => {
+		try {
+			const foundBlog = await Blog.findById(req.params.id);
+			if(foundBlog) return res.render("blogs/edit", {blog: foundBlog});
+			req.flash("error toast", "Blog no longer exists.");
+			res.redirect("/blogs");
+		} catch(err) {
+			req.flash("error toast", "An error occurred. Please try again.");
+			res.redirect("/blogs"); 
+		}
 	},
-	updateBlog : (req, res) => {
+	updateBlog : async (req, res) => {
 		req.body.blog.body = req.sanitize(req.body.blog.body);
 
-		Blog.findOneAndUpdate(req.params.id, req.body.blog)
-			.then((updatedBlog) => {
-				res.redirect("/blogs/" + req.params.id);
-			})
-			.catch((err) => {
-				req.flash("error toast", "Failed to update blog. Please try again.");
-				res.redirect("/blogs");
-			});
+		try {
+			const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body.blog);
+			if(updatedBlog) return res.redirect(`/blogs/${req.params.id}`);
+			req.flash("error toast", "Blog no longer exists.");
+			res.redirect("/blogs");
+		} catch(err) {
+			req.flash("error toast", "Failed to update blog. Please try again.");
+			res.redirect("/blogs");
+		}
 	},
 	deleteConfirm : (req, res) => {
 		res.render("blogs/delete", {blog_id: req.params.id});
 	},
-	deleteBlog : (req, res) => {
-		Blog.findById(req.params.id)
-			.then((blog) => {
-				blog.remove();
-				User.findOneAndUpdate(blog.author.id, {$pull: {blogs: blog._id}}).exec();
-				return Comment.find({_id: {$in: blog.comments}});
-			})
-			.then(comments => User.updateMany({$pull: {comments: {$in: comments}}}).exec())
-			.then(() => Comment.deleteMany({blog: {id: mongoose.Types.ObjectId(req.params.id)}}).exec())
-			.then(() => {
-				req.flash("success toast", "Blog removed.");
-				res.redirect("/");
-			})
-			.catch((err) => {
-				console.log(err);
-				req.flash("error toast", "Something went wrong!");
-				res.redirect("/");
-			});
+	deleteBlog : async (req, res) => {
+		try {
+			const blog = await Blog.findById(req.params.id);
+			blog.remove();
+			await User.findOneAndUpdate(blog.author.id, {$pull: {blogs: blog._id}});
+			const comments = await Comment.find({_id: {$in: blog.comments}});
+			await User.updateMany({$pull: {comments: {$in: comments}}});
+			await Comment.deleteMany({blog: {id: mongoose.Types.ObjectId(req.params.id)}});
+			req.flash("success toast", "Blog removed.");
+			res.redirect("/");
+		} catch(err) {
+			req.flash("error toast", "Something went wrong! Please try again.");
+			res.redirect("/");
+		}
 	}
 }
