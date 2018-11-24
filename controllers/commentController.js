52fd4c489133d5e4fd33324295642b692f30fde3
Nicholas Ewing <nicholassewing@gmail.com>
2018-11-24 15:25:20 +0000

    Rewrote comment controller to use async/await

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -3,71 +3,66 @@ const User = require("../models/user");
 const Comment = require("../models/comment");
 
 module.exports = {
-	newCommentForm : (req, res) => {
-		Blog.findById(req.params.id)
-			.then((blog) => res.render("comments/new", {blog: blog}))
-			.catch((err) => {
-				console.log(err);
-				req.flash("error toast", "Error occurred. Please try again!");
-				res.redirect("/blogs");
-			});
+	newCommentForm : async (req, res) => {
+		try {
+			const blog = await Blog.findById(req.params.id);
+			if(blog) return res.render("comments/new", {blog: blog});
+			res.redirect(`/blogs/${req.params.id}`);
+		} catch(err) {
+			req.flash("error toast", "Something went wrong! Please try again.");
+			res.redirect("/blogs");
+		}
 	},
-	createComment : (req, res) => {
-		Blog.findById(req.params.id)
-			.then((blog) => {
-				return Comment.create(req.body.comment);
-			})
-			.then((comment) => {
-				Blog.findByIdAndUpdate(req.params.id, {$push : {comments: comment }}).exec();
-				User.findByIdAndUpdate(req.user._id, {$push : {comments: comment }}).exec();
-				comment.author.username = req.user.username;
-				comment.author.id = req.user._id;
-				comment.blog.id = req.params.id;
-				comment.save();
-				res.redirect("/blogs/" + req.params.id);
-			})
-			.catch((err) => {
-				console.log(err);
-				req.flash("error toast", "An error occurred. Please try again.");
-				res.redirect("/blogs/" + req.params.id);
-			});
+	createComment : async (req, res) => {
+		try {
+			const blog = await Blog.findById(req.params.id);
+			const comment = await Comment.create(req.body.comment);
+			await Blog.findByIdAndUpdate(req.params.id, {$push : {comments: comment}});
+			await User.findByIdAndUpdate(req.user._id, {$push : {comments: comment}});
+			comment.author.username = req.user.username;
+			comment.author.id = req.user._id;
+			comment.blog.id = req.params.id;
+			comment.save();
+			res.redirect(`/blogs/${req.params.id}`);
+		} catch(err) {
+			req.flash("error toast", "An error occurred. Please try again.");
+			res.redirect(`/blogs/${req.params.id}`);
+		}
 	},
-	updateCommentForm : (req, res) => {
-		Comment.findById(req.params.comment_id)
-			.then((foundComment) => {
-				if(foundComment) {
-					return res.render("comments/edit", {blog_id: req.params.id, comment: foundComment});					
-				}
-
-				req.flash("error toast", "Comment no longer exists."); 
-				return res.redirect("/blogs/" + req.params.id);
-			})
-			.catch((err) => {
-				console.log(err);
-				req.flash("error toast", "Couldn't open edit view for this comment. Please try again.");
-			});
+	updateCommentForm : async (req, res) => {
+		try {
+			const foundComment = await Comment.findById(req.params.comment_id);
+			if(foundComment) return res.render("comments/edit", {blog_id: req.params.id, comment: foundComment});
+			req.flash("error toast", "Comment no longer exists.");
+			res.redirect(`/blogs/${req.params.id}`);
+		} catch(err) {
+			req.flash("error toast", "Couldn't open edit view for this comment. Please try again.");
+			res.redirect(`/blogs/${req.params.id}`);
+		}
 	},
-	updateComment : (req, res) => {
-		Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment)
-			.then((foundComment) => {
-				if(foundComment) {
-					return res.redirect("/blogs/" + req.params.id);
-				}
-
-				req.flash("error toast", "Comment no longer exists."); 
-				return res.redirect("/blogs/" + req.params.id);
-			});
+	updateComment : async (req, res) => {
+		try {
+			const foundComment = await Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment);
+			if(foundComment) return res.redirect(`/blogs/${req.params.id}`);
+			req.flash("error toast", "Comment no longer exists.");
+			res.redirect(`/blogs/${req.params.id}`); 
+		} catch(err) {
+			res.redirect(`/blogs/${req.params.id}`);
+		}
 	},
 	deleteConfirm : (req, res) => {
 		res.render("comments/delete", {blog_id: req.params.id, comment_id: req.params.comment_id});
 	},
-	deleteComment : (req, res) => {
-		Comment.findByIdAndRemove(req.params.comment_id)
-			.then((comment) => {
-				User.findByIdAndUpdate(comment.author.id, {$pull: {comments: {$in: req.params.comment_id}}}).exec();
-				Blog.findByIdAndUpdate(req.params.id, {$pull: {comments: {$in: comment._id}}}).exec();
-				req.flash("success toast", "Comment deleted.");
-				return res.redirect("/blogs/" + req.params.id);
-			});
+	deleteComment : async (req, res) => {
+		try {
+			const comment = await Comment.findByIdAndRemove(req.params.comment_id);
+			await User.findByIdAndUpdate(comment.author.id, {$pull: {comments: {$in: comment._id}}});
+			await Blog.findByIdAndUpdate(req.params.id, {$pull: {comments: {$in: comment._id}}});
+			req.flash("success toast", "Comment deleted.");
+			res.redirect(`/blogs/${req.params.id}`);
+		} catch(err) {
+			req.flash("error toast", "Couldn't delete comment. Please try again.");
+			res.redirect(`/blogs/${req.params.id}`);
+		}
 	}
 }
