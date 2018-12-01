96d9792e3bc683b0846423677aae5a317c530a80
Nicholas Ewing <nicholassewing@gmail.com>
2018-12-01 20:58:36 +0000

    Add icons, fade anims, and fix various CSS errors

--- public/js/script.js
+++ public/js/script.js
@@ -87,3 +87,19 @@ function closeToast() {
 		ele.parentNode.removeChild(ele);
 	}, 200);
 }
+
+function fadeIn(el) {
+  el.style.opacity = 0;
+
+  var last = +new Date();
+  var tick = function() {
+    el.style.opacity = +el.style.opacity + (new Date() - last) / 400;
+    last = +new Date();
+
+    if (+el.style.opacity < 1) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+    }
+  };
+
+  tick();
+}
